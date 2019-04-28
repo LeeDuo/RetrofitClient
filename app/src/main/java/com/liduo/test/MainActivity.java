43de930830d90cba76dec1f6699cb2f629fce28e
liduo <yichuanyanyu1991@163.com>
2019-04-28 17:30:41 +0800

    添加插件，实现上传至Bintray Maven仓库

--- app/src/main/java/com/liduo/test/MainActivity.java
+++ app/src/main/java/com/liduo/test/MainActivity.java
@@ -2,10 +2,9 @@ package com.liduo.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.liduo.retrofitclient.RetrofitClient;
-import com.liduo.retrofitclient.response.ResponseCallback;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,19 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RetrofitClient.getInstance();
 
-        //初始化RetrofitClient
-        RetrofitClient.init("http://www.baidu.com");
-        RetrofitClient.getInstance().get().setApiName("").call(new ResponseCallback<String>() {
-            @Override
-            public void onResponse(String response) {
-
-            }
-
-            @Override
-            public void onError(String errMsg) {
-
-            }
-        });
     }
 }
