729c88867cf36b363b4e561a1b820b11337108a1
liduo <yichuanyanyu1991@163.com>
2019-06-10 08:37:33 +0800

    修复post请求没有文件参数时，后台无法接收到请求的问题

--- retrofitclient/src/main/java/com/liduo/retrofitclient/request/FormRequest.java
+++ retrofitclient/src/main/java/com/liduo/retrofitclient/request/FormRequest.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
+import okhttp3.FormBody;
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
@@ -17,60 +18,77 @@ import okhttp3.RequestBody;
  * 可以上传表单参数，也可以添加上传文件
  */
 
-public class FormRequest extends BaseRequest<FormRequest>{
+public class FormRequest extends BaseRequest<FormRequest> {
 
-	private Map<String, File> fileMap;
+    private Map<String, File> fileMap;
 
-	public FormRequest(){
-		super(BaseRequest.METHOD_POST);
-		fileMap = new HashMap<>();
-	}
+    public FormRequest() {
+        super(BaseRequest.METHOD_POST);
+        fileMap = new HashMap<>();
+    }
 
-	public FormRequest(String method){
-		super(method);
-		fileMap = new HashMap<>();
-	}
+    public FormRequest(String method) {
+        super(method);
+        fileMap = new HashMap<>();
+    }
 
-	@Override
-	public void call(ResponseCallback callback){
-		MultipartBody.Builder builder = new MultipartBody.Builder();
-		if(paramMap != null && paramMap.size() > 0){
-			for(Map.Entry<String, String> entry : paramMap.entrySet()){
-				builder.addFormDataPart(entry.getKey(), entry.getValue());
-			}
-		}
-		if(fileMap != null && fileMap.size() > 0){
-			for(Map.Entry<String, File> entry : fileMap.entrySet()){
-				RequestBody fileBody = RequestBody.create(MediaType.parse("multipart/form-data"), entry.getValue());
-				builder.addFormDataPart(entry.getKey(), entry.getValue().getName(), fileBody);
-			}
-		}
-		if(method.equals(BaseRequest.METHOD_POST)){
-			ResponseHandler.getInstance().post(apiName, headerMap, builder.build(), callback);
-		}
-		if(method.equals(BaseRequest.METHOD_PUT)){
-			ResponseHandler.getInstance().put(apiName, headerMap, builder.build(), callback);
-		}
+    @Override
+    public void call(ResponseCallback callback) {
+        //没有文件参数
+        if (fileMap == null || fileMap.size() == 0) {
+            FormBody.Builder builder = new FormBody.Builder();
+            if (paramMap != null && paramMap.size() > 0) {
+                for (Map.Entry<String, String> entry : paramMap.entrySet()) {
+                    builder.add(entry.getKey(), entry.getValue());
+                }
+            }
 
+            if (method.equals(BaseRequest.METHOD_POST)) {
+                ResponseHandler.getInstance().post(apiName, headerMap, builder.build(), callback);
+            }
+            if (method.equals(BaseRequest.METHOD_PUT)) {
+                ResponseHandler.getInstance().put(apiName, headerMap, builder.build(), callback);
+            }
 
-	}
+        } else {
+            //包含文件参数
+            MultipartBody.Builder builder = new MultipartBody.Builder();
+            if (paramMap != null && paramMap.size() > 0) {
+                for (Map.Entry<String, String> entry : paramMap.entrySet()) {
+                    builder.addFormDataPart(entry.getKey(), entry.getValue());
+                }
+            }
+            for (Map.Entry<String, File> entry : fileMap.entrySet()) {
+                RequestBody fileBody = RequestBody.create(MediaType.parse("multipart/form-data"), entry.getValue());
+                builder.addFormDataPart(entry.getKey(), entry.getValue().getName(), fileBody);
+            }
+            if (method.equals(BaseRequest.METHOD_POST)) {
+                ResponseHandler.getInstance().post(apiName, headerMap, builder.build(), callback);
+            }
+            if (method.equals(BaseRequest.METHOD_PUT)) {
+                ResponseHandler.getInstance().put(apiName, headerMap, builder.build(), callback);
+            }
+        }
+    }
 
-	/**
-	 * add a file
-	 * @param key
-	 * @param file
-	 */
-	public FormRequest addFile(String key, File file){
-		fileMap.put(key, file);
-		return this;
-	}
+    /**
+     * add a file
+     *
+     * @param key
+     * @param file
+     */
+    public FormRequest addFile(String key, File file) {
+        fileMap.put(key, file);
+        return this;
+    }
 
-	/**
-	 * add a file map
-	 * @param fileMap
-	 */
-	public FormRequest addFile(Map<String, File> fileMap){
-		this.fileMap.putAll(fileMap);
-		return this;
-	}
+    /**
+     * add a file map
+     *
+     * @param fileMap
+     */
+    public FormRequest addFile(Map<String, File> fileMap) {
+        this.fileMap.putAll(fileMap);
+        return this;
+    }
 }
